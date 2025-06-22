d0141f5c5d490dbe943ccd61966a001893777818
MohammadMunnagusux <123978667+MohammadMunnagusux@users.noreply.github.com>
2025-06-22 22:46:04 +0600

    Delete entertainment-media directory

--- entertainment-media/template-parts/pagination.php
+++
@@ -1,5 +0,0 @@
-<?php
-	the_posts_pagination( array(
-		'prev_text' => esc_html__( 'Previous page', 'entertainment-media' ),
-		'next_text' => esc_html__( 'Next page', 'entertainment-media' ),
-	) );
